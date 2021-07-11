3984c66770d1c85aff4ce6b91f7666fa32a62cff
Takumikimura1899 <81891074+Takumikimura1899@users.noreply.github.com>
2021-07-11 22:10:55 +0900

    stateで画面遷移

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom";
 
 export const Page1 = () => {
+  const arr = [...Array(100).keys()];
+  console.log(arr);
+
   return (
     <div>
       <h1>Page1</h1>
       <br />
       <hr />
       <h2>To Page1 DetailA</h2>
-      <Link to="/page1/detailA">DetailA</Link>
+      <Link to={{ pathname: "/page1/detailA", state: arr }}>DetailA</Link>
       <hr />
       <h2>To Page1 DetailB</h2>
       <Link to="/page1/detailB">DetailB</Link>
